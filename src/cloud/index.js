63eb57011169ed7afd8614c9ff2ba51130ed0ff0
WinTP <1789016472@qq.com>
2023-10-07 15:56:33 +0800

    feat(cloud): add cloud module Provide cloud functions

---
+++ src/cloud/index.js
@@ -0,0 +1,22 @@
+/**
+ * 云端模块，提供存取数据库的方法
+ * @module cloud
+ */
+
+import * as dataTypes from './types'
+import Taro from '@tarojs/taro'
+
+const UserDB = {
+    _userId: -1,
+    /**
+     * 获取当前用户的ID
+     * @returns {number} 用户ID
+     */
+    getUserId() {
+        
+    }
+}
+
+export default {
+    UserDB
+}
---
+++ src/cloud/types.js
@@ -0,0 +1,14 @@
+/**
+ * 云端模块类型定义，提供数据库记录类型的JSDoc定义
+ */
+
+/**
+ * @typedef UserRecord 用户数据库记录
+ * @property {string} id 用户ID
+ * @property {string} nickname 用户昵称
+ * @property {string} avatar 用户头像
+ * @property {number} winCount 胜场数
+ * @property {number} loseCount 胜场数
+ */
+
+export default {}
