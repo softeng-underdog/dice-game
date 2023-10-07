4a71b71b1b7aa5863aeaa6eea530e14df94ad5d5
WinTP <1789016472@qq.com>
2023-10-08 00:33:51 +0800

    feat(UI): update game store Add game mode, room id and player index.

--- src/stores/game.js
+++ src/stores/game.js
@@ -6,9 +6,38 @@ import { defineStore } from 'pinia'
 import { ref } from 'vue'
 import GameData from '../game-data'
 
+/**
+ * 游戏模式枚举
+ * @readonly
+ * @enum {number}
+ */
+const GameMode = {
+  offline: 0,
+  online: 1
+}
 
 export const useGameStore = defineStore('game', () => {
+  /**
+   * 游戏数据层模块对象
+   */
   const gd = ref(new GameData())
-  
-  return { gd }
+  /**
+   * 游戏模式
+   */
+  const mode = ref(GameMode.offline)
+  /**
+   * 房间Id（仅对战模式）
+   */
+  const roomId = ref('')
+  /**
+   * 当前玩家数据索引（仅对战模式）
+   */
+  const playerIndex = ref(0)
+
+  return { 
+    gd,
+    mode,
+    roomId,
+    playerIndex
+   }
 })
