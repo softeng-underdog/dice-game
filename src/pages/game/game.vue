5ab782b03f77945aa8e0f934f64cffa178304c55
WinTP <1789016472@qq.com>
2023-10-09 00:03:59 +0800

    feat(UI): display actual data of current view player

--- src/pages/game/game.vue
+++ src/pages/game/game.vue
@@ -1,25 +1,22 @@
 <template>
   <view class="page-game">
-    <game-info avatar="cloud://cloud1-2gum4le1e2076a50.636c-cloud1-2gum4le1e2076a50-1321067110/user.png" :multiplier="1" :current-game="1" :games="12" @toggle-view="togglePlayerView" />
+    <game-info :avatar="viewPlayerData.avatar" :multiplier="gameGlobalInfo.multiplier" :current-game="gameGlobalInfo.currentGame"
+      :games="gameGlobalInfo.games" @toggle-view="togglePlayerView" />
     <view v-if="viewSelector == 0" class="game-view">
       <view class="game-area">
         <view class="free-area">
-          <image-dice class="game-dice" :dice="1" />
-          <image-dice class="game-dice" :dice="2" />
-          <image-dice class="game-dice" :dice="3" />
-          <image-dice class="game-dice" :dice="4" />
-          <image-dice class="game-dice" :dice="5" />
+          <image-dice v-for="dice of viewPlayerData.diceData" class="game-dice" :key="globalStore.getGlobalKey(dice)" :dice="dice" />
         </view>
-        <bonus-tr name="双对" :score="10" />
-        <bonus-tr name="三连" :score="10" achieved />
-        <bonus-tr name="葫芦" :score="20" />
-        <bonus-tr name="小顺子" :score="30" />
-        <bonus-tr name="四连" :score="40" />
-        <bonus-tr name="大顺子" :score="60" />
-        <bonus-tr name="五连" :score="100" />
-        <total-tr name="骰子点数和" :score="0" />
-        <total-tr name="本轮总分" :score="0" />
-        <total-tr name="当前筹码" :score="0" />
+        <bonus-tr name="双对" :score="10" :achieved="viewPlayerScoreInfo.bonusType == gdTypes.BonusType.DOUBLE_PAIR" />
+        <bonus-tr name="三连" :score="10" :achieved="viewPlayerScoreInfo.bonusType == gdTypes.BonusType.TRIPLE"/>
+        <bonus-tr name="葫芦" :score="20" :achieved="viewPlayerScoreInfo.bonusType == gdTypes.BonusType.GOURD" />
+        <bonus-tr name="小顺子" :score="30" :achieved="viewPlayerScoreInfo.bonusType == gdTypes.BonusType.SMALL_STRAIGHT" />
+        <bonus-tr name="四连" :score="40" :achieved="viewPlayerScoreInfo.bonusType == gdTypes.BonusType.QUADRUPLE" />
+        <bonus-tr name="大顺子" :score="60" :achieved="viewPlayerScoreInfo.bonusType == gdTypes.BonusType.BIG_STRAIGHT" />
+        <bonus-tr name="五连" :score="100" :achieved="viewPlayerScoreInfo.bonusType == gdTypes.BonusType.QUINTUPLE" />
+        <total-tr name="骰子点数和" :score="viewPlayerScoreInfo.diceScore" />
+        <total-tr name="本轮总分" :score="viewPlayerScoreInfo.totalScore" />
+        <total-tr name="当前筹码" :score="viewPlayerData.chips" />
         <view class="lock-area">
           
         </view>
@@ -69,11 +66,41 @@ import playerCard from '../../components/game/player-card.vue'
 import { useGlobalStore } from '../../stores/global'
 import { useGameStore } from '../../stores/game'
 import * as actTypes from '../../game-action'
+import * as gdTypes from '../../game-data/types'
 import '../../style/common.css'
 import './game.css'
 
 const gameStore = useGameStore()
 const globalStore = useGlobalStore()
+
+/**
+ * 游戏全局信息
+ * @type {gdTypes.GlobalInfo}
+ */
+const gameGlobalInfo = computed(() => {
+  return gameStore.gd.getGlobalInfo()
+})
+
+/**
+ * 当前玩家视图对应的数据索引
+ */
+const viewPlayerIndex = ref(0)
+
+/**
+ * 当前玩家视图对应的玩家数据
+ * @type {gdTypes.PlayerData}
+ */
+const viewPlayerData = computed(() => {
+  return gameStore.gd.getPlayerData(viewPlayerIndex.value)
+})
+
+/**
+ * 当前玩家视图对应的玩家分数信息
+ * @type {gdTypes.ScoreInfo}
+ */
+const viewPlayerScoreInfo = computed(() => {
+  return gameStore.gd.getPlayerScoreInfo(viewPlayerIndex.value)
+})
 
 useLoad(() => {
   Taro.hideHomeButton()
